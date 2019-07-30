72d360888040deab3063e44b5d92305b8c0a8211
Enderson Vizcaino <enjaviga@gmail.com>
2019-07-29 22:56:14 -0500

    0fe96d8

---
+++ precache-manifest.2541a4855a1e24705a5546badab4b949.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "774439635ccc39a5e903da59413b6104",
+    "url": "/portfolio/img/taxiapp.77443963.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/portfolio/robots.txt"
+  },
+  {
+    "revision": "bea042bcda99b786241d4a1e9d80fa87",
+    "url": "/portfolio/img/laravel.bea042bc.svg"
+  },
+  {
+    "revision": "23ae36421814f4ce0b09",
+    "url": "/portfolio/js/chunk-vendors.feba130e.js"
+  },
+  {
+    "revision": "3dea8abbca0487db8bed40ef6b3d784a",
+    "url": "/portfolio/img/gir.3dea8abb.jpg"
+  },
+  {
+    "revision": "cb73d9121f50651a15a1fa5ac4102f43",
+    "url": "/portfolio/img/ngrx.cb73d912.svg"
+  },
+  {
+    "revision": "7be973879041cf4e72efe6b5a8502512",
+    "url": "/portfolio/img/profile.7be97387.jpg"
+  },
+  {
+    "revision": "7634d1a3c06643e9394adc4ba35d7d2e",
+    "url": "/portfolio/img/php-1.7634d1a3.svg"
+  },
+  {
+    "revision": "50db5a235b843e764e1bc65b0c63b906",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/portfolio/img/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "156a1a19303b9e4ba048",
+    "url": "/portfolio/js/app.23469119.js"
+  },
+  {
+    "revision": "9e694fb30d22259a40b338e04bc69475",
+    "url": "/portfolio/img/unnes.9e694fb3.png"
+  },
+  {
+    "revision": "56a8edc412d29db98c9b64ed003f94f8",
+    "url": "/portfolio/img/profile2.56a8edc4.png"
+  },
+  {
+    "revision": "60dfaadbbd65a730821eb0e7b5974020",
+    "url": "/portfolio/img/vue-9.60dfaadb.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/img/react.4009eef3.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/portfolio/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f85782bcb0a8df8ce619a859f162072c",
+    "url": "/portfolio/img/ionic.f85782bc.svg"
+  },
+  {
+    "revision": "7a4cc7ee92c56cdebe9bb3b54c3db838",
+    "url": "/portfolio/img/calprome.7a4cc7ee.png"
+  },
+  {
+    "revision": "60871687e1bfa0f511aa3f0b17f3cd8d",
+    "url": "/portfolio/img/bg.60871687.png"
+  },
+  {
+    "revision": "48ae79d99d69739a8022593da1ece32f",
+    "url": "/portfolio/img/angular.48ae79d9.svg"
+  },
+  {
+    "revision": "156a1a19303b9e4ba048",
+    "url": "/portfolio/css/app.18569a1d.css"
+  }
+];
